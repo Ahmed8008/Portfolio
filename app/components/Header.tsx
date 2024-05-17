b18586a24d6d0472f3396eeb96adc09ef5b72f05
Ahmed8008 <88592717+Ahmed8008@users.noreply.github.com>
2024-05-17 15:53:31 +0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import {motion} from "framer-motion";
-import {links} from  "/app/lib/data";
+import {links} from  "@/app/lib/data";
 import Link from 'next/link';
 
 export default function Header()
