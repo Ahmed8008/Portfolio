3924675d3c63486cabd93da40860555939feb276
Ahmed8008 <88592717+Ahmed8008@users.noreply.github.com>
2024-05-17 15:55:33 +0500

    Update Intro.tsx

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import {delay, motion} from "framer-motion";
-import {links} from  "/app/lib/data";
+import {links} from  "@/app/lib/data";
 import Link from 'next/link';
 import Image from 'next/image'
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
