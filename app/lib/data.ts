a5e4a2a0b453b2447d07432eb4060717572f6a28
Ahmed8008 <88592717+Ahmed8008@users.noreply.github.com>
2024-10-16 23:31:03 +0500

    Update data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,6 +5,7 @@ import { LuGraduationCap } from "react-icons/lu";
 import Glowifybeautyimg from "/public/Glowifybeautyimg.png";
 import LesvestieHair from "/public/LesvestieHair.png";
 import UIDataAgency from "/public/UIDataAgency.png";
+import inventorymangemnetsystem from "/public/inventorymangemnetsystem.png";
 
 export const links = [
   {
