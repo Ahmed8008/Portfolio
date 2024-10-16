80e23d927e98bab9c91338b94aeca9ce00fef733
Ahmed8008 <88592717+Ahmed8008@users.noreply.github.com>
2024-10-16 23:44:30 +0500

    Update Intro.tsx

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -75,7 +75,7 @@ export default function Intro()
                   </Link>
 
                     <a className=' group bg-white  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 
-                       transition cursor-pointer text-black border-black/10 ' href='/Ahmed Resumee (1) (2) (3) (1).pdf' download>
+                       transition cursor-pointer text-black border-black/10 ' href='/Ahmed Resume.pdf' download>
                         Download Cv <HiDownload  className="opacity-60 group-hover:translate-y-1 transition"/>
                     </a>
 
