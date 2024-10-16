2a0230c7729f0116ec1a1a5cfffa714702e0c15d
Ahmed8008 <88592717+Ahmed8008@users.noreply.github.com>
2024-10-16 22:42:25 +0500

    data.ts

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -3,7 +3,7 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import Glowifybeautyimg from "/public/Glowifybeautyimg.png";
-import LesvestieHair from "/public/LesvestieHair.png";
+import LesvestieHair from "/public/LesvestieHair.PNG";
 import UIDataAgency from "/public/UIDataAgency.png";
 import inventorymangemnetsystem from "/public/inventorymangemnetsystem.png";
 import ExerciseLog from "/public/ExerciseLog.png"
